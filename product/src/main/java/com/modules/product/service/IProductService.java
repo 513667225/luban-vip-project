68ef018380f028a5687244cb662c104d8cc2f1c5
taibai <1772276596@qq.com>
2019-05-31 14:50:40 +0800

    Revert "tjx"
    This reverts commit 1061010

--- product/src/main/java/com/modules/product/service/IProductService.java
+++ product/src/main/java/com/modules/product/service/IProductService.java
@@ -4,7 +4,6 @@ import com.modules.product.entity.Product;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.modules.product.util.R;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,13 +16,7 @@ import java.util.Map;
  */
 public interface IProductService extends IService<Product> {
 
-    R gerProduct(Map<String,Object> map);
+    public R gerProduct(Map<String,Object> map);
 
-    R updateStock(Map<String,Object> map) throws Exception;
-
-    R getProductByid(String id);
-
-   List<Map<String,Object>> getAllStocks();
-
-    R getAdminProduct(Map<String, Object> map,int page,int limit);
+    public R updateStock(Map<String,Object> map) throws Exception;
 }
